9e977c60b381e40c20de74190385dc117436b546
josefyitayal <josefyitayal@gmail.com>
2025-01-12 18:31:35 +0000

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-center">Yosef Prime VX</h1>
+    </div>
+  );
+}
